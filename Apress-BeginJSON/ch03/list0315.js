2d09a4b14bc53e75cb55621e86d511b5dbf2da55
jedichou <jedigit@163.com>
2019-12-27 19:51:54 +0800

    删除空行

--- Apress-BeginJSON/ch03/list0315.js
+++ Apress-BeginJSON/ch03/list0315.js
@@ -1,6 +1,7 @@
 // Ref: Apress Beginning JSON
 // Listing 3-15. Extracting substring with substr
 // Jedi Chou, 2016.3.7 19:58
+
 var str = 'hello world';
 var strObj = new String(str);
 var startIndex = strObj.indexOf('w');
