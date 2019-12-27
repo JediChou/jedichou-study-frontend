8fdb6df4d56d78b218ecc47300da885034dc1de9
jedichou <jedigit@163.com>
2019-12-27 14:12:12 +0800

    给listing021.js增加调试页

--- Apress-BeginJSON/ch01/list0121.js
+++
@@ -1,8 +0,0 @@
-// Ref: Apress Beginning JSON
-// Listing 1-21. Substituting Block Statements for Statements
-// Jedi Chou, 2016.3.7 14:41
-
-var bol = false;
-if (bol) { console.log('condition is met'); }
-else { console.log('condition is not met'); }
-
---
+++ Apress-BeginJSON/ch01/listing0121/list0121.js
@@ -0,0 +1,7 @@
+// Ref: Apress Beginning JSON
+// Listing 1-21. Substituting Block Statements for Statements
+// Jedi Chou, 2016.3.7 14:41
+
+var bol = false;
+if (bol) { console.log('condition is met'); alert('condition is met');}
+else { console.log('condition is not met'); alert('condition is not met');}
