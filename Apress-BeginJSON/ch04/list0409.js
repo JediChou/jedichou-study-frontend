1b9720d41c5c225145f9201a4d7efff7a6bf5cdc
jedichou <jedigit@163.com>
2019-12-27 19:56:22 +0800

    修正代码错误

--- Apress-BeginJSON/ch04/list0409.js
+++ Apress-BeginJSON/ch04/list0409.js
@@ -3,7 +3,7 @@
 // Jedi Chou, 2016.3.7 20:29
 
 console.log(-0.01);				// valid use of 0's
-console.log(00.1);				// superfluous 0 produces a SyntaxError
+console.log(0.1);				// superfluous 0 produces a SyntaxError
 console.log(1/3);				// fraction form
 console.log(.33333333333333);	// decimal form
-console.log(1.23-1);			// scientific notation
+console.log(1.2e-1);			// scientific notation
