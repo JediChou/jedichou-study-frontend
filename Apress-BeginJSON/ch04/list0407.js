4a5bfbd41103a162e93574c76d0c151049086804
jedichou <jedigit@163.com>
2019-12-27 19:55:42 +0800

    增加相关输出

--- Apress-BeginJSON/ch04/list0407.js
+++ Apress-BeginJSON/ch04/list0407.js
@@ -2,6 +2,7 @@
 // Listing 4-7. 
 //   the very first json message used by douglas crockford
 // Jedi Chou, 2016.3.7 20:21
+// Jedi Chou, add other outputs. 2019.12.27
 
 // syntax error in ECMA 3
 var firstJSON = {
@@ -10,3 +11,14 @@ var firstJSON = {
 	message:'hello world'
 };
 console.log(firstJSON);
+
+// get object properties
+console.log(firstJSON.to);
+console.log(firstJSON.do);
+console.log(firstJSON.message);
+
+// Get object keys with Object.keys property
+Object.keys(firstJSON).forEach(function(key){
+	val = firstJSON[key];
+	console.log(key, '->', val);
+});
