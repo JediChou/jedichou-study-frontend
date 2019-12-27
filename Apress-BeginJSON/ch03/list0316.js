a2948530796ece59884ff02b789ef355c06bc729
jedichou <jedigit@163.com>
2019-12-27 19:52:30 +0800

    增加说明代码

--- Apress-BeginJSON/ch03/list0316.js
+++ Apress-BeginJSON/ch03/list0316.js
@@ -1,5 +1,10 @@
 // Ref: Apress Beginning JSON
 // Listing 3-16. separating a comma-delimited string
 // Jedi Chou, 2016.3.7 20:01
+
 var strObj = new String('ben,mike,ivan,kyle');
 console.log(strObj.split(','));
+
+// Jedi: It is not a good practice
+for(var index in strObj.split(','))
+    console.log(index, strObj.split(',')[index]);
