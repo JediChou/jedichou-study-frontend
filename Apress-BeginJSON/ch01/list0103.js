037ee6ef7b8c15ee183ee2db1d6a7f7e5c19fe95
jedichou <jedigit@163.com>
2019-12-27 14:06:27 +0800

    最后一行增加备注

--- Apress-BeginJSON/ch01/list0103.js
+++ Apress-BeginJSON/ch01/list0103.js
@@ -1,7 +1,7 @@
 // Ref: Apress Beginning JSON
-// Listing 1-3. Valid encodings of the string type
+// Listing 1-3. Valid String value
 // Jedi Chou, 2016.3.7 13:08
 console.log("this is a string value");
 console.log("string");
 console.log("s");
-console.log("");
+console.log(""); // An empty String
