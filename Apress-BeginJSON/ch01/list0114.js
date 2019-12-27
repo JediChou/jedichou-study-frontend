620f31e8ee52c513b7fef16b7ce3b81173aeec26
jedichou <jedigit@163.com>
2019-12-27 14:07:12 +0800

    增加其他对象的输出并说明目的

--- Apress-BeginJSON/ch01/list0114.js
+++ Apress-BeginJSON/ch01/list0114.js
@@ -1,6 +1,7 @@
 // Ref: Apress Beginning JSON
 // Listing 1-14. Determing Data Types
 // Jedi Chou, 2016.3.7 14:13
+// Jedi Chou, 2019.12.27 11:34 AM
 
 console.log(typeof 3);				// outputs number
 console.log(typeof "hello world");	// outputs string
@@ -8,3 +9,9 @@ console.log(typeof true);			// outputs boolean
 console.log(typeof  new Object());	// outputs object
 var emptyVariable;
 console.log(typeof emptyVariable);	// outpus undefined
+
+// null, undefined 
+console.log(typeof null);           // Notice: null is a object
+console.log(typeof undefined);      // Notice: undefined just is undefined
+console.log(typeof {});
+console.log(typeof []);
