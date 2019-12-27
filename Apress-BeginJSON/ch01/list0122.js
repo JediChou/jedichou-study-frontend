f2025d5063d59997fd4ba0ecc5854f004c08c0d0
jedichou <jedigit@163.com>
2019-12-27 14:12:36 +0800

    排版格式化

--- Apress-BeginJSON/ch01/list0122.js
+++ Apress-BeginJSON/ch01/list0122.js
@@ -2,9 +2,9 @@
 // Listing 1-22. Demonstrates All Falsy Values
 // Jedi Chou, 2016.3.7 14:45
 
-if ('')		console.log('an empty string');
-if (0)		console.log('the number 0');
-if (null)	console.log('a value of null');
-if (false)	console.log('a value of false');
-if (undefined) console.log('a value of undefined');
-if (NaN)	console.log('a value of NaN');
+if ('')		    console.log('an empty string');
+if (0)		    console.log('the number 0');
+if (null)	    console.log('a value of null');
+if (false)	    console.log('a value of false');
+if (undefined)  console.log('a value of undefined');
+if (NaN)	    console.log('a value of NaN');
