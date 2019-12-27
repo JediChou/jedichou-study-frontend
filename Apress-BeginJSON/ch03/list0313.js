beeae7f12c344ca5d0d90c54dbd52be8b9f4a3b2
jedichou <jedigit@163.com>
2019-12-27 19:50:47 +0800

    增加注释和相关调试代码

--- Apress-BeginJSON/ch03/list0313.js
+++ Apress-BeginJSON/ch03/list0313.js
@@ -1,8 +1,12 @@
 // Ref: Apress Beginning JSON
 // Listing 3-13. replacing matched substring
 // Jedi Chou, 2015.3.7 19:44
+// Jedi Chou, add a line to explain String object special feature. 
+//            2019.12.27 16:25
+
 var str = 'hello world';
 var strObj = new String(str);
 var result = strObj.replace('hello', 'goodbye');
 console.log(result);
-console.log(strObj);
+console.log(strObj);  // String Object
+console.log(strObj.toString());  // String Object's toString method executed result
